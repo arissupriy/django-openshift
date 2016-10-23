e6a9c9c5bf1616b27e9143955fd1d106397593d7
Aris Supriyanto <arissy96@gmail.com>
2016-10-23 16:28:07 +0700

    Evidence

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Evidence Repository',
+      version='1.0',
+      description='Evidence Repository',
+      author='Aris Supriyanto',
+      author_email='arissy96@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+#      install_requires=['Django>=1.3'],
+     )
